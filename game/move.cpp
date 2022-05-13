42dfa47cde2b2e850d6492cb1273b2d9671b6b27
Marianne Johnson <joh20041@byui.edu>
2022-05-12 21:57:45 -0700

    Saved changes to move file.

--- game/move.cpp
+++ game/move.cpp
@@ -3,19 +3,5 @@
 using namespace std;
 
 class Move {
-    public:
-        //Declare functions
-        void apply();
-        bool check_win();
-        string to_string();
-    
-    private:
-        //Declare functions
-        void prepare();
-        //Declare variables
-        string code;
-        string guess;
-        string player_1_status[2];
-        string player_2_status[2];
-        int turn = 0;
+
 };
